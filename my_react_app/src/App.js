bd0fab6adac1ac4f9464a2fb7483a2121058a8e1
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-05-30 23:39:43 -0300

    changed the app to a counter, understood working with onClick listeners in React.js

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,39 +1,14 @@
 import './App.css';
-
-const Person = (props) =>{
-  return (
-    <>
-    <h1>First Name: {props.FName}</h1>
-    <h2>Last Name: {props.LName}</h2>
-    <h2>Age: {props.Age}</h2>
-    </>
-  );
-}
-
-const Button = () =>{
-  return (
-    <Button >
-      Let's Chat
-    </Button>
-  )
-}
+import {useState, useEffect} from 'react';
 const App = () => { 
-  const name = "John";
-  const isNameShowing = true;
+  const[counter, setCounter] = useState(0);
+  useEffect(() =>{
+    },[]);
   return (
     <div className="App">
-      <Person 
-        FName={"John"} 
-        LName ={"Doe"} 
-        Age={25}
-      />
-      <Button />
-      <Person
-        FName= {"Mary"} 
-        LName={"Doe"} 
-        Age={24}
-      />
-      <Button />
+      <button onClick={() => {setCounter((prevCount) => prevCount-1)}}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => {setCounter((prevCount => prevCount+1))}}>+</button>
     </div>
   );
 }  
