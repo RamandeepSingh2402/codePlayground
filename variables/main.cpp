4fbdd6966c1fb62dd8085cc2958c76619f3531b7
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-06-02 17:57:12 -0300

    learnt about the working of substring, and the .find() method

--- variables/main.cpp
+++ variables/main.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 int main() {
 
-    char grade = 'A';
-    string phrase = "Cpp gang";
-    int age = 50;
-    double gpa= 3.60 ;
-    bool isMale = true;
+//    char grade = 'A';
+//    string phrase = "Cpp gang";
+//    int age = 50;
+//    double gpa= 3.60 ;
+//    bool isMale = true;
 
     // A double can store more decimal points than a float data type
+//
+//    cout<<isMale<< endl;
 
-    cout<<isMale<< endl;
+       string phrase = "Giraffe Academy";
+
+       cout<<phrase.find("Academy")<<endl;
+       string animal = phrase.substr(0,7);
+       cout<<animal<<endl;
+       cout<<phrase.substr(0,7);
+
 
     return 0;
 }
