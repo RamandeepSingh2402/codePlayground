0ebe8c32f11fe7a1a4c3be501ca8be5c5ffe9250
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-06-02 12:10:02 -0300

    learnt using the string and integer variables in cpp

---
+++ variables/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string characterName = "John";
+    int characterAge = 70;
+    cout<<"There was a man named " << characterName << endl;
+    cout<<"He was " << characterAge <<" years old"<<endl;
+    cout<<"He liked the name "<< characterName <<endl;
+    cout<<"He did not like being " << characterAge <<endl;
+    return 0;
+}
