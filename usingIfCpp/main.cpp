0a06ab44264e40c82db2848ea2523c80170aa83a
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-06-06 17:14:43 -0300

    used if statements using the comparisons

--- usingIfCpp/main.cpp
+++ usingIfCpp/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 using  namespace std;
+
+int getMax(int num1, int num2, int num3){
+    int result;
+    if(num1>=num2 && num1>=num3){
+        result = num1;
+    } else if(num2>=num1 && num2>=num3){
+        result = num2;
+    }else{
+        result = num3;
+    }
+    return result;
+}
 
 int main() {
     // bool isMale = false;
@@ -16,7 +29,7 @@ int main() {
     // }
 
     //If statements contd
-    
+    cout<<getMax(2,5,2);
     return 0;
 
 }
