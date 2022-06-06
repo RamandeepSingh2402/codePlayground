7dc631ac6ce49014afc67be45d58bdc5eb2f07d5
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-06-06 18:56:16 -0300

    learnt to use the for, and the for-each loop

--- loopsInCpp/main.cpp
+++ loopsInCpp/main.cpp
@@ -3,11 +3,36 @@
 using namespace std;
 
 int main() {
-    int index =6;
-    do{
-        cout<< index << endl;
+    int index = 0;
+
+    //while loop
+    while(index<=5){
+        cout<<index<<" ";
         index++;
     }
-    while(index<=5);
+
+    cout<<endl;
+
+    index = 6;
+
+    //for loop
+    for(int i = 0; i<index; i++){
+        cout<< i << " ";
+    }
+
+    cout<<endl;
+
+    // using for loop for GOOD
+    int nums[] = {1,2,5,7,3};
+    for(int i = 0; i<5; i++){
+        cout<<nums[i]<<" ";
+    }
+
+    cout<<endl;
+
+    //using for-each loop as well
+    for(int num : nums){
+        cout<<num<<" ";
+    }
     return 0;
 }
