13f38cb26f827f7176c6cf552ea72306748977ca
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-06-06 17:24:58 -0300

    used the if statements to improve the existing calculator project

--- calculatorCPP/main.cpp
+++ calculatorCPP/main.cpp
@@ -4,13 +4,34 @@ using namespace std;
 
 int main() {
     double num1, num2;
-    cout<<"Enter the first number:";
+    char op;
+
+    cout<< "Enter first number: ";
     cin>>num1;
 
-    cout<<"Enter the second number:";
+    cout<< "Enter operator: ";
+    cin>>op;
+
+    cout<<"Enter the second number: ";
     cin>>num2;
 
-    cout<<"The sum of the two numbers is: "<< num1+num2;
+    double result;
+    if(op == '+'){
+        result = num1 + num2;
+    }
+    else if(op == '-'){
+        result = num1 - num2;
+    }
+    else if(op == '/'){
+        result = num1 / num2;
+    }
+    else if(op == '*'){
+        result = num1 * num2;
+    }
+    else{
+        cout << "Invalid Operator";
+    }
+    cout<<"The result of the desired calculation is: "<< result;
 
     return 0;
 }
